76f6b4f0e5eb195529b9a2d62c00916486ac1d9d
Loic CHOLLIER <loic@chollier.com>
2014-09-04 15:38:46 -0700

    Put all the requires in index.js
    otherwise it'd lead to quirky circular dependencies behavior

--- index.js
+++ index.js
@@ -1,9 +1,54 @@
 FormBuilder = {
 
-  Mixins: require('./mixins/Mixins'),
-  Containers: require('./containers/Containers'),
-  Controls: require('./controls/Controls'),
-  Validators: require('./validators/Validators')
+  Mixins: {
+    FormHandlerMixin: require('./mixins/FormHandlerMixin'),
+
+    DataSourcedMixin: require('./mixins/FormDataSourcedMixin'),
+    DataTypeConversionMixin: require('./mixins/FormDataTypeConversionMixin'),
+    DisableOnSubmitMixin: require('./mixins/FormDisableOnSubmitMixin'),
+    HelpMixin: require('./mixins/FormHelpMixin'),
+    ParserMixin: require('./mixins/FormParserMixin'),
+    SingleInputMixin: require('./mixins/FormSingleInputMixin'),
+    StandardErrorDisplayMixin: require('./mixins/FormStandardErrorDisplayMixin'),
+    TypeAheadMixin: require('./mixins/FormTypeAheadMixin'),
+    ValidationMixin: require('./mixins/FormValidationMixin'),
+    ConfirmationMixin: require('./mixins/ConfirmationMixin')
+
+  },
+  Containers: {
+    Form: require('./containers/Form'),
+    DynamicDefinition: require('./containers/FormDynamicDefinition'),
+    Group: require('./containers/FormGroup'),
+    InputGroup: require('./containers/FormInputGroup')
+
+  },
+  Controls: {
+    Checkbox: require('./controls/FormCheckbox'),
+    DateField: require('./controls/FormDateField'),
+    DateTimeField: require('./controls/FormDateTimeField'),
+    DropdownField: require('./controls/FormDropdownField'),
+    FileSelect: require('./controls/FormFileSelect'),
+    HiddenField: require('./controls/FormHiddenField'),
+    MultipleSelect: require('./controls/FormMultipleSelect'),
+    NestedField: require('./controls/FormNestedField'),
+    NestedFieldGroup: require('./controls/FormNestedFieldGroup.jsx'),
+    NestedFormGroup: require('./controls/FormNestedFormGroup.jsx'),
+    NumberField: require('./controls/FormNumberField'),
+    PasswordField: require('./controls/FormPasswordField'),
+    RadioButtons: require('./controls/FormRadioButtons'),
+    TextArea: require('./controls/FormTextArea'),
+    TextField: require('./controls/FormTextField'),
+    MultiTypeAheadField: require('./controls/FormMultiTypeAheadField'),
+    TypeAheadField: require('./controls/FormTypeAheadField'),
+    MarkdownField: require('./controls/FormMarkdownField.jsx')
+
+  },
+  Validators: {
+    FilenameValidator: require('./validators/FilenameValidator'),
+    LengthValidator: require('./validators/LengthValidator'),
+    NotNullValidator: require('./validators/NotNullValidator'),
+    NumericValidator: require('./validators/NumericValidator')
+  }
 
 }
 
